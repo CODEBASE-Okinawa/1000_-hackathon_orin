a8a00fb0885c9cd66312ea771b0e0088304d93ca
osatoh <sugar961216@gmail.com>
2022-07-16 12:02:11 +0900

    modify task model

--- db/migrate/20220716010722_create_tasks.rb
+++ db/migrate/20220716010722_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.integer :status
+      t.boolean :complete
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 20.times do |n|
-  Task.create!(title: "タスク#{n + 1}", status: [0, 1].sample)
+  Task.create!(title: "タスク#{n + 1}", complete: [true, false].sample)
 end
