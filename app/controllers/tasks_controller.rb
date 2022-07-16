eee5ad56e1548ccf7118f689609ba4a0b07df275
rin0000 <wta15214@nuhw.ac.jp>
2022-07-16 22:09:27 +0900

    add tasks form,complete default

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,19 @@ class TasksController < ApplicationController
     @tasks_not_yet = Task.where(complete: false)
     @tasks_done = Task.where(complete: true)
   end
+
+  def new
+    @task = Task.new
+    render 'index'
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:title)
+    end
 end
