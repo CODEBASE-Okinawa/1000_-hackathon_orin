2dc68f2619796e88c15c01240d1fcdfde88dee0a
osatoh <sugar961216@gmail.com>
2022-07-17 13:59:45 +0900

    add comment model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
-20.times do |n|
-  Task.create!(title: "タスク#{n + 1}", complete: [true, false].sample)
+10.times do |i|
+  User.create!(name: "test user#{i + 1}", email: "test#{i + 1}@example.com", password: "password", password_confirmation: "password")
+  Task.create!(title: "タスク#{i + 1}", complete: [true, false].sample)
+end
+
+100.times do |n|
+  Comment.create!(description: "コメント#{n + 1}", task_id: (1..10).to_a.sample, user_id: (1..10).to_a.sample)
 end
